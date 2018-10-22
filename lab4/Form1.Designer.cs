c5f045333b9ade69bedc60a524c295c2d12a0ecc
Vladimir Osm <volodia-osm@rambler.ru>
2018-10-22 23:10:34 +0300

    Add files via upload

---
+++ lab4/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace lab4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(3, 2);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(783, 546);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(953, 93);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(95, 63);
+            this.button2.TabIndex = 20;
+            this.button2.Text = "нарисовать оболочку";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(953, 162);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(95, 63);
+            this.button1.TabIndex = 21;
+            this.button1.Text = "удалить все";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ lab4/Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab4
+{
+
+
+
+    public partial class Form1 : Form
+    {
+        Graphics g;
+        Bitmap im;
+        Point p;
+        Brush MyBrush;
+        int w, h;
+        List<Point> contur= new List<Point>() ;
+        ////////////////////////////////////список точек ребра и многоугольника///////////////////////////
+        List<Point> list_polygon = new List<Point>();
+        List<Point> edge = new List<Point>(2);
+        Point pt = new Point(300,300);
+        List<Point> edge2 = new List<Point>(2);
+
+ 
+
+
+        bool cmp(Point a, Point b)
+        {
+            return a.X < b.X || a.X == b.X && a.Y < b.Y;
+        }
+
+        bool cw(Point a, Point b, Point c)
+        {
+            return a.X * (b.Y - c.Y) + b.X * (c.Y - a.Y) + c.X * (a.Y - b.Y) < 0;
+        }
+
+        bool ccw(Point a, Point b, Point c)
+        {
+            return a.X * (b.Y - c.Y) + b.X * (c.Y - a.Y) + c.X * (a.Y - b.Y) > 0;
+        }
+
+        void sort(List<Point>  a)
+        {
+            for (int i = 0; i < a.Count; i++)
+            {
+                for (int j = i+1; j < a.Count; j++)
+                {
+                    if (! cmp(a[i], a[j]))
+                    {
+                        Point t = a[i];
+                        a[i] = a[j];
+                        a[j] = t;
+                    }
+                }
+            }
+        }
+        void convex_hull(List<Point> a)
+        {
+            if (a.Count == 1) return;
+            
+            sort(a);
+            Point p1 = a[0], p2 = a.Last();
+            List<Point> up = new List<Point>();
+            List<Point> down = new List<Point>();
+            up.Add(p1);
+            down.Add(p1);
+            for (int i = 1; i < a.Count(); ++i)
+            {
+                if (i == a.Count() - 1 || cw(p1, a[i], p2))
+                {
+                    while (up.Count() >= 2 && !cw(up[up.Count() - 2], up[up.Count() - 1], a[i]))
+                        up.RemoveAt(up.Count() - 1);
+                    up.Add(a[i]);
+                }
+                if (i == a.Count() - 1 || ccw(p1, a[i], p2))
+                {
+                    while (down.Count() >= 2 && !ccw(down[down.Count() - 2], down[down.Count() - 1], a[i]))
+                        down.RemoveAt(down.Count() - 1);
+                    down.Add(a[i]);
+                }
+            }
+            a.RemoveRange(0, a.Count());
+            for (int i = 0; i < up.Count(); ++i)
+                a.Add(up[i]);
+            for (int i = down.Count() - 2; i > 0; --i)
+                a.Add(down[i]);
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            KeyPreview = true;
+
+            im = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            g = Graphics.FromImage(im);
+            pictureBox1.Image = im;
+            w = pictureBox1.Width;
+            h = pictureBox1.Height;
+
+            MyBrush = Brushes.LightGray;
+            p.X = -1;
+            p.Y = -1;
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {           
+                pt.X = e.X;
+                pt.Y = e.Y;
+                contur.Add(new Point(e.X, e.Y));
+                g.DrawLine(new Pen(Color.Black, 5), pt.X - 3, pt.Y - 3, pt.X + 3, pt.Y + 3);
+                g.DrawLine(new Pen(Color.Black, 5), pt.X - 3, pt.Y + 3, pt.X + 3, pt.Y - 3);               
+                pictureBox1.Refresh();            
+        }
+
+
+
+       
+        private void Erase_graphics()
+        {
+            pictureBox1.Image = null;
+            im = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            g = Graphics.FromImage(im);
+            pictureBox1.Image = im;
+        }
+
+
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (contur.Count() > 1)
+            {
+                convex_hull(contur);
+                for (int i = 0; i < contur.Count() - 1; ++i)
+                {
+                    g.DrawLine(new Pen(Color.Green, 4), contur[i].X, contur[i].Y, contur[i + 1].X, contur[i + 1].Y);
+                }
+                g.DrawLine(new Pen(Color.Green, 4), contur[contur.Count() - 1].X, contur[contur.Count() - 1].Y, contur[0].X, contur[0].Y);
+                pictureBox1.Refresh();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Erase_graphics();
+            contur.RemoveRange(0, contur.Count()); ;
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+            {
+                pictureBox1.Image = null;
+                im = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+                g = Graphics.FromImage(im);
+                pictureBox1.Image = im;
+            }
+        }
+    }
+}
